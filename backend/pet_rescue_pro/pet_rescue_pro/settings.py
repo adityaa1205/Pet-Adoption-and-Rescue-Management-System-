ba09f1099f29b67b4bf9196f69f64333ac580878
adityaa1205 <145536614+adityaa1205@users.noreply.github.com>
2025-10-17 15:16:19 +0530

    settings

--- backend/pet_rescue_pro/pet_rescue_pro/settings.py
+++ backend/pet_rescue_pro/pet_rescue_pro/settings.py
@@ -109,8 +109,13 @@ USE_I18N = True
 USE_TZ = True
 
 # ==== Static & Media ====
-STATIC_URL = 'static/'
+# ==== Static & Media ====
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
